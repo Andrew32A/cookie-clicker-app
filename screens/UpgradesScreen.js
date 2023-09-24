7bc26527094aff0e9d653c2350b505e6f92fd415
Andrew Alsing <andrew32alsing@gmail.com>
2023-09-24 15:29:01 -0700

    added upgrades

--- screens/UpgradesScreen.js
+++ screens/UpgradesScreen.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, FlatList } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function UpgradesScreen() {
+  const dispatch = useDispatch();
+  const cookies = useSelector((state) => state.cookies);
+  const upgrades = useSelector((state) => state.upgrades);
+
+  const purchaseUpgrade = (id, price, owned) => {
+    if (cookies >= price * (owned + 1)) {
+      dispatch({ type: "PURCHASE_UPGRADE", id: id });
+    }
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Upgrades Screen</Text>
-      {/* TODO: add upgrades here */}
-    </View>
+    <FlatList
+      data={upgrades}
+      renderItem={({ item }) => (
+        <View
+          style={{
+            padding: 15,
+            borderBottomWidth: 1,
+            borderBottomColor: "#ccc",
+          }}
+        >
+          <Text>
+            {item.name} ({item.owned} owned)
+          </Text>
+          <Text>Cost: {item.basePrice * (item.owned + 1)} cookies</Text>
+          <Text>Effect: +{item.multiplier} cookies per click</Text>
+          <Button
+            title={`Buy ${item.name}`}
+            onPress={() => purchaseUpgrade(item.id, item.basePrice, item.owned)}
+          />
+        </View>
+      )}
+    />
   );
 }
--- store.js
+++ store.js
@@ -2,12 +2,39 @@ import { createStore } from "redux";
 
 const initialState = {
   cookies: 0,
+  upgrades: [
+    { id: "1", name: "Auto Clicker", multiplier: 1, basePrice: 10, owned: 0 },
+    { id: "2", name: "Grandma Baker", multiplier: 2, basePrice: 20, owned: 0 },
+    // TODO: add more upgrades here
+  ],
 };
 
 function cookieReducer(state = initialState, action) {
   switch (action.type) {
     case "INCREMENT_COOKIE":
-      return { ...state, cookies: state.cookies + 1 };
+      const incrementValue = state.upgrades.reduce(
+        (total, upgrade) => total + upgrade.multiplier * upgrade.owned,
+        1
+      );
+      return { ...state, cookies: state.cookies + incrementValue };
+    case "PURCHASE_UPGRADE":
+      const upgradeIndex = state.upgrades.findIndex(
+        (up) => up.id === action.id
+      );
+      if (
+        state.cookies >=
+        state.upgrades[upgradeIndex].basePrice *
+          (state.upgrades[upgradeIndex].owned + 1)
+      ) {
+        const newCookies =
+          state.cookies -
+          state.upgrades[upgradeIndex].basePrice *
+            (state.upgrades[upgradeIndex].owned + 1);
+        const newUpgrades = [...state.upgrades];
+        newUpgrades[upgradeIndex].owned++;
+        return { ...state, cookies: newCookies, upgrades: newUpgrades };
+      }
+      return state;
     default:
       return state;
   }
