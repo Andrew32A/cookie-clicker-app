129d9d507b7c19b61bed5a6e56aa31f82288db19
Andrew Alsing <andrew32alsing@gmail.com>
2023-09-26 15:59:05 -0700

    added spinning animation to cookie

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,43 @@
-import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, Text, Image, TouchableOpacity, Animated } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
   const cookies = useSelector((state) => state.cookies);
 
+  const rotation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const spinAnimation = Animated.loop(
+      Animated.timing(rotation, {
+        toValue: 1,
+        duration: 20000,
+        useNativeDriver: true,
+      })
+    );
+
+    spinAnimation.start();
+
+    return () => spinAnimation.stop();
+  }, [rotation]);
+
+  const rotationInterpolate = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>{cookies} Cookies</Text>
       <TouchableOpacity onPress={() => dispatch({ type: "INCREMENT_COOKIE" })}>
-        <Image
+        <Animated.Image
           source={require("../assets/cookie.webp")}
-          style={{ width: 300, height: 300 }}
+          style={{
+            width: 300,
+            height: 300,
+            transform: [{ rotate: rotationInterpolate }],
+          }}
         />
       </TouchableOpacity>
     </View>
